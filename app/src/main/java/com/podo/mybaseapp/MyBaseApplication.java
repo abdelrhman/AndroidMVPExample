1399f68dc7a05279f7dbdaa6be85913befe14777
Abdelrhman Talat <abdelrhman@arabhosters.com>
2015-10-09 20:27:36 +0200

    init commit with butterknife

---
+++ app/src/main/java/com/podo/mybaseapp/MyBaseApplication.java
@@ -0,0 +1,14 @@
+package com.podo.mybaseapp;
+
+import android.app.Application;
+
+/**
+ * Created by podo on 10/9/15.
+ */
+public class MyBaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
