db7b50e765c266989f5f1a7b3a7209353b379eab
Abdelrhman Talat <abdelrhman@arabhosters.com>
2015-10-09 22:21:37 +0200

    Using Lombok

--- app/src/main/java/com/podo/mybaseapp/MyBaseApplication.java
+++ app/src/main/java/com/podo/mybaseapp/MyBaseApplication.java
@@ -3,11 +3,14 @@ package com.podo.mybaseapp;
 import android.app.Application;
 import android.content.Context;
 
+import lombok.Getter;
+
 /**
  * Created by podo on 10/9/15.
  */
 public class MyBaseApplication extends Application {
 
+    @Getter
     private  AppComponent appComponent;
 
     public static MyBaseApplication get(Context context){
@@ -18,14 +21,10 @@ public class MyBaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         initAppComponent();
-
     }
 
     private void initAppComponent() {
         appComponent = DaggerAppComponent.builder().appModule(new AppModule(this)).build();
     }
 
-    public AppComponent getAppComponent() {
-        return appComponent;
-    }
 }
